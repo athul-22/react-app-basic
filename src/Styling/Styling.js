a5efd3e96fe7a8df176642a5414a34e54442f585
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-01-16 18:31:36 +0530

    CSS STYLING

---
+++ src/Styling/Styling.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Styling.css'
+
+class Styling  extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className='text'>COLOURED TEXT</h3>
+      </div>
+    )
+  }
+}
+
+export default Styling 
