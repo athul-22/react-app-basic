b6bfd472f1cf079f42c09d78919b26bad7375e05
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-01-23 20:19:13 +0530

    V3

--- src/Compoents/RefsDemo.js
+++ src/Compoents/RefsDemo.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 
  class RefsDemo extends Component {
+
+
+    // CREATING AUTOFOCUS OF INPUT WITH REFS
+
+    constructor(props) {
+      super(props)
+        this.inputRef = React.createRef()
+    }
+
+    componentDidMount(){
+        this.inputRef.current.focus()
+        console.log(this.inputRef)
+    }
+
   render() {
     return (
       <div>
-        <center><input className='input-refs'></input></center>
+        <center><input className='input-refs' ref={this.inputRef}></input></center>
       </div>
     )
   }
