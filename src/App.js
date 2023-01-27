cfb8be91c32bd247b735f2b7187f2638ce05bee1
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-01-27 19:02:53 +0530

    Hook Input

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import RefsDemo from './Compoents/RefsDemo';
 import ApiComponent from './Compoents/ApiComponent';
 import HookCounter from './HooksComponent/HookCounter';
 import HooksCounter2 from './HooksComponent/HooksCounter2';
+import HookInput from './HooksComponent/HookInput';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
     {/* HOOKS*/}
       <HooksCounter2/>
       <HookCounter/>
+      <HookInput/>
     {/* BEGINEER - INTERMEDIATE */}
      <Welcome/>
      <Text/>
--- src/HooksComponent/HookInput.js
+++ src/HooksComponent/HookInput.js
@@ -6,8 +6,10 @@ const[name , setName] = useState({firstName : '', lastName: ''})
 
   return (
     <div>
-        <input type="text" value={name.firstName} onChange={e = ()=> setName({firstName: e.target.value })}/>
-
+        <input type="text" value={name.firstName} onChange={e => setName({...name,firstName: e.target.value })}/>
+        <input type="text" value={name.lastName} onChange={a => setName({...name,lastName : a.target.value})} />
+        <h3>YOUR NAME IS : {name.firstName}</h3>
+        <h3>LAST NAME IS : {name.lastName}</h3>
     </div>
   )
 }
