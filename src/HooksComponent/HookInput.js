ac527ea61867a0481de505aa8b3b942cf9a2e72c
Athul Nambiar <108534940+athul-22@users.noreply.github.com>
2023-08-25 23:21:18 +0530

    Update HookInput.js

--- src/HooksComponent/HookInput.js
+++ src/HooksComponent/HookInput.js
@@ -5,6 +5,7 @@ function HookInput() {
 const[name , setName] = useState({firstName : '', lastName: ''})
 
   return (
+    
     <div>
         <input type="text" value={name.firstName} onChange={e => setName({...name,firstName: e.target.value })}/>
         <input type="text" value={name.lastName} onChange={a => setName({...name,lastName : a.target.value})} />
